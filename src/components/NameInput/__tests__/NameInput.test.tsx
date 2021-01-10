fe16fb89f1a520137c46b683f229cffb81f398db
Dinos <dinos.vlachantonis@gmail.com>
2021-01-10 11:21:35 +0100

    failing test after hardlink validation fixed

--- src/components/NameInput/__tests__/NameInput.test.tsx
+++ src/components/NameInput/__tests__/NameInput.test.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { render } from "../../../test-utils";
+import { render, initialState } from "../../../test-utils";
 import {NameInput} from "../NameInput";
+import {State} from '../../../types'
 
+const state: State = {
+  ...initialState,
+  newWidget: {
+    language: 'Greek',
+    name: 'Doge',
+    id: -1
+  }
+}
 describe("<NameInput />", () => {
   test("should display an empty name input with a create button", async () => {
-    const { asFragment } = render(<NameInput />);
+    const { asFragment } = render(<NameInput />, {initialState: state});
     expect(asFragment()).toMatchSnapshot()
   });
 });
